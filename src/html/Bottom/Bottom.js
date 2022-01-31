ea7b15e87cf57ec313dc15d03c5c4b1b77579c7b
zazubairahmad <zazubairahmad@gmail.com>
2022-01-31 09:41:20 +0500

    hi

---
+++ src/html/Bottom/Bottom.js
@@ -0,0 +1,21 @@
+import img1 from './pic/Testimonial.png'
+
+function Bottom() {
+    return (
+
+        <div className="container-fluid">
+        <div className="row">
+        <div className="col">
+        <img className='TESTIMONIALS' src={img1} alt="" />
+        </div>
+        </div>
+
+        <div className="row">
+        <div className="col">
+        </div>
+        </div>
+</div> 
+    );
+  }
+  
+  export default Bottom;
